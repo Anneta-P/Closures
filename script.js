89f97772b65fba6e16df9627ff66b95c1bf36ccd
AkaBussola <akabussola@Users-MacBook-Pro-2.local>
2023-07-20 15:38:29 +0300

    new commit

---
+++ script.js
@@ -0,0 +1,88 @@
+// const DB = {
+//     collections: {
+//       posts: {
+//         posts_1: {
+//           name: 'Post 1',
+//           text: 'Some text 1'
+//         },
+//         posts_2: {
+//           name: 'Post 2',
+//           text: 'Some text 2'
+//         }
+//       },
+//       comments: {
+//         comments_1: {
+//           postId: 'post_1',
+//           text: 'Comment 1'
+//         },
+//         comments_2: {
+//           postId: 'post_1',
+//           text: 'Comment 2'
+//         }
+//       }
+//     }
+//   };
+  
+//   const useCollection = coll => {
+//     // ...
+//   };
+
+//   const getPostItem = useCollection('posts');
+
+// const item1 = getPostItem('posts_1');
+// console.log(item1);
+// /*
+// {
+//   id: 'posts_1',
+//   name: 'Post 1',
+//   text: 'Some text 1'
+// }
+// */
+
+// const notFoundItem = getPostItem('posts_4');
+// console.log(notFoundItem);
+// /*
+// null
+// */
+
+
+function useCollection(collectionName) {
+
+  const fakeDatabase = {
+    posts: [
+      { id: 'posts_1', name: 'Post 1', text: 'Some text 1' },
+      { id: 'posts_2', name: 'Post 2', text: 'Some text 2' },
+      // Інші записи...
+    ],
+  };
+
+ 
+  function getPostItem(id) {
+   
+    const collection = fakeDatabase[collectionName];
+
+  
+    for (const item of collection) {
+      if (item.id === id) {
+        return item;
+      }
+    }
+
+
+    return null;
+  }
+
+
+  return getPostItem;
+}
+
+const getPostItem = useCollection('posts');
+
+
+const item1 = getPostItem('posts_1');
+console.log(item1);
+
+
+const notFoundItem = getPostItem('posts_4');
+console.log(notFoundItem);
+
