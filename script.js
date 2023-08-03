3005589de4ba0efefce4211d925bc47bf96af4f9
AkaBussola <akabussola@Users-MBP-2.bbrouter>
2023-08-03 11:50:54 +0300

    new commit

--- script.js
+++ script.js
@@ -48,35 +48,55 @@ const DB = {
 // */
 
 
-function useCollection(collectionName) {
+// function useCollection(collectionName) {
 
+//   const fakeDatabase = {
+//     posts: [
+//       { id: 'posts_1', name: 'Post 1', text: 'Some text 1' },
+//       { id: 'posts_2', name: 'Post 2', text: 'Some text 2' },
+    
+//     ],
+//   };
+
+ 
+//   function getPostItem(id) {
+   
+//     const collection = fakeDatabase[collectionName];
+
+  
+//     for (const item of collection) {
+//       if (item.id === id) {
+//         return item;
+//       }
+//     }
+
+
+//     return null;
+//   }
+
+
+//   return getPostItem;
+// }
+const useCollection = collectionName => {
   const fakeDatabase = {
     posts: [
       { id: 'posts_1', name: 'Post 1', text: 'Some text 1' },
       { id: 'posts_2', name: 'Post 2', text: 'Some text 2' },
-    
     ],
   };
 
- 
-  function getPostItem(id) {
-   
+  const getPostItem = id => {
     const collection = fakeDatabase[collectionName];
-
-  
     for (const item of collection) {
       if (item.id === id) {
         return item;
       }
     }
-
-
     return null;
-  }
-
+  };
 
   return getPostItem;
-}
+};
 
 const getPostItem = useCollection('posts');
 
