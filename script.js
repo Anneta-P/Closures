0f493a63466af8e8b7ff3b4622cabdb2c68dd329
AkaBussola <akabussola@Users-MacBook-Pro-2.local>
2023-08-10 00:34:36 +0300

    new commit

--- script.js
+++ script.js
@@ -25,37 +25,37 @@ const DB = {
 
 
 
+// ЗАВДАННЯ 1
+// const useCollection = collectionName => {
+//   const fakeDatabase = {
+//     posts: [
+//       { id: 'posts_1', name: 'Post 1', text: 'Some text 1' },
+//       { id: 'posts_2', name: 'Post 2', text: 'Some text 2' },
+//     ],
+//   };
 
-const useCollection = collectionName => {
-  const fakeDatabase = {
-    posts: [
-      { id: 'posts_1', name: 'Post 1', text: 'Some text 1' },
-      { id: 'posts_2', name: 'Post 2', text: 'Some text 2' },
-    ],
-  };
+//   const getPostItem = id => {
+//     const collection = fakeDatabase[collectionName];
+//     for (const item of collection) {
+//       if (item.id === id) {
+//         return item;
+//       }
+//     }
+//     return null;
+//   };
 
-  const getPostItem = id => {
-    const collection = fakeDatabase[collectionName];
-    for (const item of collection) {
-      if (item.id === id) {
-        return item;
-      }
-    }
-    return null;
-  };
+//   return getPostItem;
+// };
 
-  return getPostItem;
-};
-
-const getPostItem = useCollection('posts');
-
-
-const item1 = getPostItem('posts_1');
-console.log(item1);
+// const getPostItem = useCollection('posts');
 
 
-const notFoundItem = getPostItem('posts_4');
-console.log(notFoundItem);
+// const item1 = getPostItem('posts_1');
+// console.log(item1);
+
+
+// const notFoundItem = getPostItem('posts_4');
+// console.log(notFoundItem);
 
 // function useCollection(collectionName) {
 
@@ -87,3 +87,13 @@ console.log(notFoundItem);
 //   return getPostItem;
 // }
 
+
+
+
+// ЗАВДАННЯ 2
+
+const collection = useCollection('posts');
+collection.get('some-id');
+collection.create(data);
+collection.deleteOne('some-id');
+collection.edit('some-id', dataToEdit);
