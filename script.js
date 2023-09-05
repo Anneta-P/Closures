cc762771d2581d0bd2c70cf4290b1df7a3513a18
AkaBussola <akabussola@Users-MacBook-Pro-2.local>
2023-09-05 21:19:14 +0300

    first answer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 const newCounter = () => {
 let value = 0;
   const counter = () =>{
@@ -36,3 +36,43 @@ for(let i = 10; i < subscribers.lenght; i++){
   console.log(subscribers[i])
 }
 
+*/
+
+const DB = {
+  collections: {
+    posts: {
+      posts_1: {
+        name: 'Post 1',
+        text: 'Some text 1'
+      },
+      posts_2: {
+        name: 'Post 2',
+        text: 'Some text 2'
+      }
+    },
+    comments: {
+      comments_1: {
+        postId: 'post_1',
+        text: 'Comment 1'
+      },
+      comments_2: {
+        postId: 'post_1',
+        text: 'Comment 2'
+      }
+    }
+  }
+};
+
+const useCollection = coll => {
+
+  const getPostItem = useCollection('collections');
+  const item = getPostItem('posts_1');
+console.log(item);
+  return function(){
+    console.log(posts);
+    console.log(posts_1);
+  }
+};
+
+const post = useCollection();
+post();
